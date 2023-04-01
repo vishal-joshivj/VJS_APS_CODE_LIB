17c4c721ad2bc614ce3d22320aa6b16284d70645
VJ <83571378+vishal-joshivj@users.noreply.github.com>
2023-04-02 01:42:14 +0530

    Add files via upload

---
+++ linked_list_cycle_06.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+class Solution
+{
+public:
+  bool hasCycle(ListNode *head)
+  {
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast and fast->next)
+    {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast)
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};  
