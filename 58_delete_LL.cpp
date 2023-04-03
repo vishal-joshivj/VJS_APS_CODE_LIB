886cc10f045c3209835bdbab0a01d2f7b752a7b6
VJ <83571378+vishal-joshivj@users.noreply.github.com>
2023-04-03 18:05:43 +0530

    Add files via upload

---
+++ 58_delete_LL.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+
+// linked_list
+
+class Solution
+{
+public:
+  void deleteNode(ListNode *node)
+  {
+    node->val = node->next->val;
+    node->next = node->next->next;
+  }
+};
